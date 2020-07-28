e178dc4b5af20883c3fd0a0715a7b7aa8f8c0c9c
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2020-07-28 19:20:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #
 # Install wipri package/command 
 # Makes wipri a common Linux command
+# Optionally makes systemd unique identity at boot (only if yes is answered at end ques)
 #
 echo "Creating /etc/wipri directory..."
 sudo mkdir /etc/wipri
@@ -18,3 +19,20 @@ sleep .5
 echo "We are done!"
 echo ""
 echo "simply type wipri (as root) or sudo wipri to begin using"
+echo ""
+sleep .25
+echo "Would you like to add wipri to systemd? (yes/no)"
+echo "This will start a new unique identity (mac/hostname) for your wifi device at each boot."
+read -p "Start a new disinfo identity at each boot (yes/no)?: " boot
+if [ $boot == yes ]; then
+do
+    cp wipri.service /etc/systemd/system/wipri.service
+    systemctl daemon-reload
+    systemctl enable wipri.service
+    systemctl start wipri.service
+    echo "WiPri has been added to each boot." 
+    echo "To stop/disable issue at boot: systemctl stop wipri && systemctl disable wipri"
+    echo "Enjoy!"
+done
+else
+
